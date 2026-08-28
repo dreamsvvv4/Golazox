9084d6b6a992e7a44be3743734a2d932159b55a2
dreamsvvv4 <dreamsvvv4@users.noreply.github.com>
2026-08-28 12:33:27 +0200

    Export renderIntroCombined

--- match_engine/webapp/_agenda_screenshot.js
+++ match_engine/webapp/_agenda_screenshot.js
@@ -634,4 +634,4 @@ if (require.main === module) {
   run(arg).then(()=>process.exit(0)).catch(e=>{ console.error(e); process.exit(1); });
 }
 
-module.exports = { run, pickEventsFor, buildHtml, renderHtmlToPngMp4, renderIntroXfade, renderIntroKenBurns, renderIntroBrandBlink, renderHtmlToPng };
+module.exports = { run, pickEventsFor, buildHtml, renderHtmlToPngMp4, renderIntroXfade, renderIntroKenBurns, renderIntroBrandBlink, renderIntroCombined, renderHtmlToPng };
